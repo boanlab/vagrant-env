2d9ea32011ec75967e8e8a84dcdb7861bd720904
Jaehyun Nam <jn@accuknox.com>
2022-07-26 16:26:18 +0000

    update setup.sh
    Signed-off-by: Jaehyun Nam <jn@accuknox.com>

--- setup.sh
+++ setup.sh
@@ -9,6 +9,11 @@ if [ ! -x "$(command -v make)" ]; then
     sudo apt-get -y install build-essential
 fi
 
+if [ ! -x "$(command -v wget)" ]; then
+    sudo apt-get update
+    sudo apt-get -y install wget
+fi
+
 if [ ! -x "$(command -v vboxmanage)" ]; then
     # install virtualbox
     wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
@@ -22,10 +27,10 @@ fi
 
 if [ ! -x "$(command -v vagrant)" ]; then
     # install vagrant
-    wget https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_x86_64.deb
-    sudo dpkg -i vagrant_2.2.9_x86_64.deb
-    sudo apt-get -y install nfs-kernel-server
-    rm vagrant_2.2.9_x86_64.deb
+    wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo apt-get update
+    sudo apt-get -y install vagrant
 
     # install vagrant plugins
     vagrant plugin install vagrant-vbguest
